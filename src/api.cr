4b990f37eeb6c438e6f4ac738ddf85c9a09f7f05
Exybore <exybore@becauseofprog.fr>
2019-11-02 22:02:36 +0100

    feat: add http client
    Added the HTTP client that allows the library to call the API.

--- src/api.cr
+++ src/api.cr
@@ -1,3 +1,5 @@
+require "http"
+
 module CrystalWeather
   # API is the interface that allows caller to interact with OpenWeatherMap's API
   class API
@@ -16,11 +18,43 @@ module CrystalWeather
     end
 
     # Fetches the current weather for a specific location
-    def current(location : String) : Current
+    def current(location : String) : Types::Current
+      data = make_request("weather", location)
+      puts data
+      return Types::Current.new
     end
 
     # Fetches the forecast for a specific location
-    def forecast(location : String) : Forecast
+    def forecast(location : String) : Types::Forecast
+      data = make_request("forecast", location)
+      puts data
+      return Types::Forecast.new
+    end
+
+    private def make_request(type : String, location : String) : JSON::Any
+      params = HTTP::Params.new
+      params.add("APPID", @api_key)
+      params.add("q", location)
+      params.add("lang", @lang)
+      params.add("units", @unit)
+
+      url = URI.parse("#{API_URL}/#{type}?#{params}")
+      response = HTTP::Client.get(url)
+      data = JSON.parse(response.body)
+
+      if response.success?
+        return data
+      else
+        api_status_text = "API status : #{data["message"]}"
+        case response.status_code
+        when 404
+          raise Exceptions::NotFound.new("unknown location #{location}. #{api_status_text}")
+        when 403
+          raise Exceptions::Unauthorized.new("unauthorized key. #{api_status_text}")
+        else
+          raise Exceptions::Unauthorized.new("unknown error from the API : #{api_status_text}")
+        end
+      end
     end
   end
 end
